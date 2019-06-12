5de738f623b76998826bdc2ef9dc659050f0ea2c
timcornwell <realtimcornwell@gmail.com>
2019-06-12 12:16:12 +0100

    Time series simulations

---
+++ arl_simulation/simulation20/do_sim.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+cp ../pointing_simulation.py .
+python pointing_simulation.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
+--static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show True --seed 18051955 --scale 1.0 1.0 --use_agg True \
+--use_radec False --integration_time 10 --time_range -0.28194 0.28194 --pbtype MID_GAUSS
