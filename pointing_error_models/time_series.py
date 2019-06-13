b2091a3d0d7b3a0e0213ca5f82f9699e0c9058b1
Hartley <p.hartley@skatelescope.org>
2019-06-13 17:17:04 +0100

    Change scaling factor

--- pointing_error_models/time_series.py
+++ pointing_error_models/time_series.py
@@ -1,14 +1,19 @@
-import numpy as np, os
+import numpy as np, os, sys
 from matplotlib import pyplot as plt
 from scipy import optimize
+
 
 
 psd = np.loadtxt("PSD_data/El45Az135.dat")
 
 plotdir = "plots/"
+outdir = "out/"
 
 if not os.path.isdir(plotdir):
     os.system('mkdir '+plotdir)
+
+if not os.path.isdir(outdir):
+    os.system('mkdir '+outdir)
 
 # inspect psds
 plt.subplot(221)
@@ -38,25 +43,44 @@ axesdict =    {
   "pel": psd[:,4]
 }
 
+doplot = 1 # turn off for speed
+
 axes = ["az","el","pxel", "pel"]
+
+
+time_stamps = 65 # currently need to increase (reduce) freq_interval to reduce (increase) number of time series points
 
 for axis in axes:
     print ('*** Creating times series from PSD of %s axis ***'%axis)
     az = axesdict[axis]
 
     freq_interval = 0.001
+
+    # the orginal PSD need to be resampled in order to obtain fixed frequency intervals fot the IFFT
+    # each PSD is broken into two parts in order to produce better polynomial fits to the data
+    # the az and el PSDs have a peak value mid-spectrum; the break is therefore located near this peak (trial and error to find best break)
+    # the pxel and pel PSDs do not have a peak mid-spectrum, so a break is chosen at an arbitrary freequency after trial and error
     if (axis=="az") or (axis=="el"):   
         # determine index of maximum PSD value; add 50 for better fit
         az_max_index = np.argwhere(az==np.max(az))[0][0]+50
+        # the maximum frequency is chosen to represent the top of the range of significant power; this may need to change for different PSDs
+        # at low power values there is a danger of the fit producing negative values at the re-sampling stage
         max_freq = 0.4
-
         freq_max_index = np.argwhere(freq>max_freq)[0][0]
     else: 
-        az_max_index = 400 # not max; just a break
-        print ('frequency break: ', az_max_index)
+        break_freq = 0.01 # not max; just a break
+        az_max_index = np.argwhere(freq>break_freq)[0][0]
+        # the maximum frequency is chosen to represent the top of the range of significant power; this may need to change for different PSDs
+        # at low power values there is a danger of the fit producing negative values at the re-sampling stage
         max_freq = 0.1
         freq_max_index = np.argwhere(freq>max_freq)[0][0]     
-        print ('max frequency: ', max_freq)   
+
+    print ('Frequency break: ', freq[az_max_index])
+    print ('Max frequency: ', max_freq)  
+
+    if az_max_index>=freq_max_index:
+        print ('Frequency break is higher than highest frequency; select a lower break')    
+        sys.exit()
 
     # fit polynomial to psd up to max value
     p_az1 = np.polyfit(freq[:az_max_index], az[:az_max_index], 5)
@@ -64,6 +88,9 @@ for axis in axes:
     # fit polynomial to psd beyond max value
     p_az2 = np.polyfit(freq[az_max_index:freq_max_index], az[az_max_index:freq_max_index], 5)
     f_az2 = np.poly1d(p_az2)
+
+
+    #freq_interval2 = (freq[freq_max_index]-freq[0])/time_stamps
 
     # resampled to construct regularly-spaced frequencies
     regular_freq1 = np.arange(freq[0], freq[az_max_index], freq_interval)
@@ -73,14 +100,19 @@ for axis in axes:
     regular_freq = np.append(regular_freq1, regular_freq2)
     regular_az = np.append(regular_az1,regular_az2)
 
-    # check sampling matches psd
-    plt.clf()
-    plt.semilogx(freq, (az), label = 'same')
-    plt.semilogx(regular_freq, regular_az, label = 'regular')
-    plt.ylabel('PSD, as^2/Hz')
-    plt.xlabel('f, Hz')
-    plt.legend()    
-    plt.savefig(plotdir+'sampled_psd_%s.png'%axis)
+    if (regular_az<0).any():
+        print ('Resampling returns negative power values; change fit range')
+        sys.exit()
+
+    if doplot:
+        # check sampling matches psd
+        plt.clf()
+        plt.semilogx(freq, (az), label = 'same')
+        plt.semilogx(regular_freq, regular_az, label = 'regular')
+        plt.ylabel('PSD, as^2/Hz')
+        plt.xlabel('f, Hz')
+        plt.legend()    
+        plt.savefig(plotdir+'sampled_psd_%s.png'%axis)
 
     # get amplitudes from psd values
     amp_az = np.sqrt(regular_az) 
@@ -94,6 +126,8 @@ for axis in axes:
     # create complex array
     z_az = amp_az * np.exp(1j*phi_az) # polar
 
+    # construct Hermitian PSD in order to produce real signal
+ 
     # make symmetrical frequencies
     mirror_z_az = np.copy(z_az)
 
@@ -108,52 +142,65 @@ for axis in axes:
     regular_freq = np.append(regular_freq,mirror_regular_freq[::-1])
 
     # add a 0 Fourier term
-    z_az = np.append(0+0*1j,z_az)
+    z_az = np.append(0.* np.exp(1j*0.),z_az) # 0 DC component since mean 0 of time series(?)
     regular_freq = np.append(0, regular_freq)
+    # set up and check scalings
+    M=len(z_az)
+    print ('Number of frequencies: ', M) # N=M+1
+
+    z_az*=np.sqrt(M)
 
     # perform inverse fft
     ts = np.fft.ifft(z_az) 
 
-    # set up and check scalings
-    M=len(regular_freq)
-    print ('Number of frequencies: ', M) # N=M+1
 
     N = len(ts)
     print ('number of times: ', N)
+
 
     Fmax = np.max(regular_freq)
     print ('Fmax: ',Fmax)
 
     Dt = 1/Fmax
     print ('Dt: ',Dt )
-
+#
     tmax = (N-1)*Dt
     print('tmax: ', tmax)
     print ()
 
-    ts.real*=N # the result is scaled by number of points in the signal, so multiply - real part - by this
+    #ts.real*=np.sqrt(N) # the result is scaled by number of points in the signal, so multiply - real part - by this
+  # see https://docs.scipy.org/doc/numpy/reference/routines.fft.html for scaling desciption
+  # ! this is wrong - appears to be scaled by 1/sqrt(N) - maybe since hermitian?
+
 
     # The output of the iFFT will be a random time series on the finite 
     # (bounded, limited) time interval t = 0 to tmax = (N-1) X Dt, #
-    # where Dt = 1 / (2 X Fmax)
+    # where Dt = 1 / (Fmax)
 
     # scale to time interval
     times = np.arange(len(ts))*Dt
 
-    # plot results
-    plt.clf()
-    plt.plot( times, np.real(ts), label = '%s axis'%axis)
-    plt.xlabel('time, s')
-    plt.ylabel('pointing error, as')
-    plt.legend()
-    plt.savefig(plotdir+'time_series_%s.png'%axis)
+    if doplot:
+
+        # plot results
+        plt.clf()
+        plt.plot( times, np.real(ts), label = '%s axis'%axis)
+        plt.xlabel('time, s')
+        plt.ylabel('pointing error, as')
+        plt.legend()
+        plt.title(  'r.m.s. = %s'%(np.std(np.real(ts))))
+        plt.savefig(plotdir+'time_series_%s.png'%axis)
     
-    # check fft returns original PSD (amplitudes plotted again)
-    ts.real/=np.float(N)
-    fft =  np.fft.fft(ts)
-    plt.clf()
-    plt.scatter(np.arange(len(fft)), np.absolute(fft), label = '%s axis'%axis) 
-    plt.xlabel('mode')
-    plt.ylabel('amplitude, as/SQRT(Hz)')
-    plt.legend()
-    plt.savefig(plotdir+'check_FFT_%s.png'%axis)
+    np.save(outdir+'time_series_%s'%axis, ts.real)
+
+    if doplot:
+        # check fft returns original PSD (amplitudes plotted again)
+        ts.real/=np.sqrt(N)
+        fft =  np.fft.fft(ts)
+
+        plt.clf()
+        plt.scatter(np.arange(len(fft)), (np.absolute(fft))**2, label = '%s axis'%axis) 
+        plt.xlabel('mode')
+        plt.ylabel('amplitude, as/SQRT(Hz)')
+        plt.legend()
+        plt.savefig(plotdir+'check_FFT_%s.png'%axis)
