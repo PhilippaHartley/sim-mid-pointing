5c7854cf60c6cb9144a3b68c3a298294efa508f8
timcornwell <realtimcornwell@gmail.com>
2019-06-19 12:48:39 +0100

    Don’t copy in do_sim.sh

--- arl_simulation/simulation25/do_sim.sh
+++ arl_simulation/simulation25/do_sim.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-cp ../pointing_simulation.py .
-python pointing_simulation.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
+
+python ../pointing_simulation_distributed.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
 --static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show True --seed 18051955 --scale 1.0 1.0 --use_agg True \
 --use_radec False --integration_time 10 --time_range -6 6 --pbtype MID_GAUSS \
 --memory 32 --time_series wind
--- arl_simulation/simulation27/do_sim.sh
+++ arl_simulation/simulation27/do_sim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-cp ../pointing_simulation_distributed.py .
-python pointing_simulation_distributed.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
+
+python ../pointing_simulation_distributed.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
 --integration_time 10 --static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show True --seed 18051955 --memory 32
