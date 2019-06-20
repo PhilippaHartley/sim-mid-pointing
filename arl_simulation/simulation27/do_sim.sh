017f59fab162217e75e20d5f5e23e485cf6303ae
timcornwell <realtimcornwell@gmail.com>
2019-06-20 13:24:53 +0100

    First version of big case

--- arl_simulation/simulation27/do_sim.sh
+++ arl_simulation/simulation27/do_sim.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
 python ../pointing_simulation_distributed.py --context singlesource --rmax 1e5 --flux_limit 1.0 --ngroup 128 \
---integration_time 10 --static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show True --seed 18051955 --memory 32
+--static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show False --seed 18051955 --scale 1.0 1.0 --use_agg True \
+--use_radec False --integration_time 1 --time_range -6 6 --pbtype MID_GAUSS \
+--memory 32 --time_series wind
