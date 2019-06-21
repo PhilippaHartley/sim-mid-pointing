2df1cb0d648e9feb898c522a70f75d2882df7058
timcornwell <realtimcornwell@gmail.com>
2019-06-21 08:41:22 +0100

    Change flux limit in sim8

--- arl_simulation/simulation8/do_sim.sh
+++ arl_simulation/simulation8/do_sim.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-python ../pointing_simulation_distributed.py --context s3sky --rmax 1e5 --flux_limit 0.1 --ngroup 128 \
+python ../pointing_simulation_distributed.py --context s3sky --rmax 1e5 --flux_limit 0.3 --ngroup 128 \
 --static_pe 0.0 --dynamic_pe 1.0 --nworkers 8 --show True --seed 18051955 --pbtype MID_GAUSS
