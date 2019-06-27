dc1b6faa120121ee56f23592675c875d1e91826b
timcornwell <realtimcornwell@gmail.com>
2019-06-27 09:10:18 +0100

    sim29 now 30s

--- arl_simulation/simulation29/do_sim.sh
+++ arl_simulation/simulation29/do_sim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-python ../pointing_simulation_distributed.py --context s3sky --rmax 1e5 --flux_limit 3 --ngroup 8 \
---nworkers 16 --show True --seed 18051955  --pbtype MID_GAUSS --memory 8  --integration_time 100 \
+python ../pointing_simulation_distributed.py --context s3sky --rmax 1e5 --flux_limit 0.3 --ngroup 8 \
+--nworkers 16 --show True --seed 18051955  --pbtype MID_GAUSS --memory 8  --integration_time 30 \
 --use_agg True --time_series wind --time_chunk 1800 --reference_pointing True | tee pointing_simulation.log
