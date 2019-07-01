5426abfac6c17239ed2eaa3db882c92cf0466ae8
timcornwell <realtimcornwell@gmail.com>
2019-07-01 09:50:31 +0100

    Update sim29 slurm

--- arl_simulation/golden_sim0/do_sim.sh
+++ arl_simulation/golden_sim0/do_sim.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python ../pointing_simulation_distributed.py --context s3sky --rmax 1e5 --flux_limit 0.3 --ngroup 8 \
---nworkers 8 --show True --seed 18051955  --pbtype MID_GAUSS --memory 16  --integration_time 10 \
+--nworkers 8 --show True --seed 18051955  --pbtype MID_GAUSS --memory 16  --integration_time 30 \
 --use_agg True --time_series wind --time_chunk 180 --reference_pointing True | tee pointing_simulation.log
