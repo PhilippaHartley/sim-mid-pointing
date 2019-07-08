29999134e1997a94a99592d538aa9c9e585206ae
Hartley <p.hartley@skatelescope.org>
2019-07-08 09:37:31 +0100

    Add EMSS beam models

---
+++ beam_models/EMSS/load_emss.py
@@ -0,0 +1,189 @@
+import scipy.io
+from matplotlib import pyplot as plt
+import numpy as np, os
+from mpl_toolkits.axes_grid1 import AxesGrid
+
+
+from astropy.io import fits
+
+     
+def cart2pol(x,y):
+    r = np.sqrt(x**2 +y**2)
+    theta = np.arctan2(x,y)
+    return(r,theta)
+
+
+freq = 965
+beamsize = 512 # even number
+
+
+D = scipy.io.loadmat("SKA_B2_%d.mat"%freq)
+th = D["th"].squeeze() # angle from bore sight with th=0 defining bore sight [deg]
+ph = D["ph"].squeeze() # angle in aperture plane with 0="up from vertex" and defines plane of symmetry [deg]
+JHH = D["Jqh"].squeeze() # Jones "voltage" pattern, arranged rows:th x columns:ph
+JVV = D["Jpv"].squeeze()
+JHV = D["Jqv"].squeeze()
+JVH = D["Jph"].squeeze()
+
+# keep theta in deg?
+print (ph.shape,th.shape)
+ph_rad = (ph/360.)*(2*np.pi)
+th_rad = (th/360.)*(2*np.pi)
+
+uflat = np.array([])
+vflat = np.array([])
+for counti, i in enumerate(th):
+    for countj, j in enumerate(ph_rad):
+        v = ((i)*np.sin(j))
+        u = ((i)*np.cos(j))
+        uflat = np.append(uflat, u)
+        vflat = np.append(vflat,v)
+
+tharr, pharr = np.meshgrid((th), ph_rad) 
+
+x, y = np.meshgrid(np.linspace(np.min(vflat),np.max(vflat), np.int(beamsize/2)), np.linspace(np.min(uflat),np.max(uflat), beamsize))
+print (x,y)
+
+
+r, theta = cart2pol(x,y)
+
+print (uflat, vflat)
+
+azel = np.zeros((4,np.int(beamsize/2),np.int(beamsize)), dtype = complex)
+
+poldict =    {
+      "JHH": JHH,
+      "JVV": JVV,
+      "JHV": JHV,
+      "JVH": JVH
+    }
+
+pols = ["JHH","JVV","JHV", "JVH"]
+
+for k , pol in enumerate(pols):
+    for i in range(beamsize):
+        for j in range(np.int(beamsize/2)):
+            azel[k,j,i]= ((poldict[pol][(np.abs(r[i,j]-th).argmin(),np.abs(theta[i,j]-ph_rad).argmin())]))
+
+
+flipped_azel = np.flip(azel, 1)             
+azel = np.hstack((flipped_azel,azel))
+
+hdu_new = fits.PrimaryHDU((np.real(azel)))
+hdu_new.writeto('real_%s.fits'%freq)
+
+hdu_new = fits.PrimaryHDU((np.imag(azel)))
+hdu_new.writeto('imag_%s.fits'%freq)
+
+hdu_new = fits.PrimaryHDU((np.absolute(azel)))
+hdu_new.writeto('amp_%s.fits'%freq)
+
+hdu_new = fits.PrimaryHDU((np.angle(azel)))
+hdu_new.writeto('phas_%s.fits'%freq)
+
+plt.clf()
+
+
+
+
+
+fig, axes = plt.subplots(nrows=2, ncols=2, sharex = True, sharey = True, figsize = (4,6))
+for count, ax in enumerate(axes.flat):
+    amp_azel = np.absolute(azel)
+    # db = 10*np.log((amp_azel)/np.max(amp_azel)) # power dB not field
+    im = ax.imshow(np.angle(poldict[pols[count]]), vmin = -np.pi, vmax = np.pi, origin = 'lower', extent = [0,180, 0, 6], aspect = (30*501)/73)
+    if (count==2) or (count==3):
+        ax.set_xlabel(r'$theta$ [deg]')
+    if (count==0) or (count==2):
+        ax.set_ylabel(r'$\phi$ [deg]')
+    ax.set_title(pols[count])    
+
+plt.tight_layout()
+fig.colorbar(im, ax=axes.ravel().tolist(), use_gridspec=True)
+plt.savefig('amplitudes_pol.png')
+
+
+
+
+
+
+
+
+
+
+amp_azel_JVH = np.absolute(azel[2,:,:])
+db_JHH = 10*np.log((amp_azel_JVH)/np.max(amp_azel_JVH))
+
+
+
+amp_azel = np.absolute(azel)
+
+fig, axes = plt.subplots(nrows=2, ncols=2, sharex = True, sharey = True, figsize = (8,6))
+for count, ax in enumerate(axes.flat):
+    amp_azel = np.absolute(azel)
+    db = 10*np.log((amp_azel)/np.max(amp_azel)) # power not voltage
+    im = ax.imshow((db[count,:,:]), vmin = -70, vmax = 0, origin = 'lower', extent = [-6, 6, -6, 6])
+    if (count==2) or (count==3):
+        ax.set_xlabel(r'$\rho$cos($\phi$) [deg]')
+    if (count==0) or (count==2):
+        ax.set_ylabel(r'$\rho$sin($\phi$) [deg]')
+    ax.set_title(pols[count])  
+
+plt.tight_layout()
+fig.colorbar(im, ax=axes.ravel().tolist(), use_gridspec=True)
+plt.savefig('amplitudes_azel.png')
+
+
+hdr = fits.Header.fromstring("""\
+SIMPLE  =                    T / conforms to FITS standard                      
+BITPIX  =                  -32 / array data type                                
+NAXIS   =                    4 / number of array dimensions                     
+NAXIS1  =                  %s                                                  
+NAXIS2  =                  %s                                                  
+NAXIS3  =                    4                                                  
+NAXIS4  =                    1                                                  
+WCSAXES =                    4 / Number of coordinate axes                      
+CRPIX1  =                 %s / Pixel coordinate of reference point            
+CRPIX2  =                 %s / Pixel coordinate of reference point            
+CRPIX3  =                  1.0 / Pixel coordinate of reference point            
+CRPIX4  =                  1.0 / Pixel coordinate of reference point            
+CDELT1  =                   %s / [deg] Coordinate increment at reference point  
+CDELT2  =                   %s / [deg] Coordinate increment at reference point  
+CDELT3  =                 -1.0 / Coordinate increment at reference point        
+CDELT4  =         999999.99999 / [Hz] Coordinate increment at reference point   
+CUNIT1  = 'deg'                / Units of coordinate increment and value        
+CUNIT2  = 'deg'                / Units of coordinate increment and value        
+CUNIT4  = 'Hz'                 / Units of coordinate increment and value        
+CTYPE1  = 'AZELGEO long'       / Coordinate type code                           
+CTYPE2  = 'AZELGEO lati'       / Coordinate type code                           
+CTYPE3  = 'STOKES'             / Coordinate type code                           
+CTYPE4  = 'FREQ'               / Frequency (linear)                             
+CRVAL1  =                  0.0 / [deg] Coordinate value at reference point      
+CRVAL2  =                  0.0 / [deg] Coordinate value at reference point      
+CRVAL3  =                 -5.0 / Coordinate value at reference point            
+CRVAL4  =         %s / [Hz] Coordinate value at reference point       
+LONPOLE =                180.0 / [deg] Native longitude of celestial pole       
+LATPOLE =                -35.0 / [deg] Native latitude of celestial pole        
+END         
+ """%(beamsize,beamsize,beamsize/2,(beamsize/2)+1, -(np.max(th)/beamsize), (np.max(th)/beamsize), (freq*1000)), sep='\n')
+
+print (hdr)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        #sin theta is radius
+        # phi is angle around 
